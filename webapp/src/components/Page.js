f882b3b0b0a5b2677f60c8860344d1a70c5ed353
shubham860 <shubham.chauhan860@gmail.com>
2019-11-18 01:17:49 +0530

    commit-5

--- webapp/src/components/Page.js
+++ webapp/src/components/Page.js
@@ -15,7 +15,6 @@ constructor(props){
     two : false,
     three : false,
     four : false,
-    array : ['Active Noise Cancellation for immersive sound.','Transparency mode for hearing what’s happening around you.','A customizable fit for all-day comfort.','Magic like you’ve never heard.']
   }
 
 }
@@ -27,15 +26,37 @@ constructor(props){
  _handle_Scroll_offSet = () => {
    var Scrollpos = window.pageYOffset;
    console.log(Scrollpos)
-   if(Scrollpos > 500 && Scrollpos < 740)
+   if(Scrollpos > 500 && Scrollpos < 650)
    {
      this.setState({
        one : true,
+       two : false,
+       three : false,
+       four : false,
      })
    }
-   else{
+   else if(Scrollpos > 651 && Scrollpos < 800){
      this.setState({
        one : false,
+       two : true,
+       three : false,
+       four : false,
+     })
+   }
+   else if(Scrollpos > 801 && Scrollpos < 950){
+     this.setState({
+       one : false,
+       two : false,
+       three : true,
+       four : false,
+     })
+   }
+   else if(Scrollpos > 951 && Scrollpos < 1100){
+     this.setState({
+       one : false,
+       two : false,
+       three : false,
+       four : true,
      })
    }
  }
@@ -43,6 +64,9 @@ constructor(props){
 
   render () {
     var one = this.state.one ? "withopacity" : "withoutopacity";
+    var two = this.state.two ? "withopacity" : "withoutopacity";
+    var three = this.state.three ? "withopacity" : "withoutopacity";
+    var four = this.state.four ? "withopacity" : "withoutopacity";
     console.log(this.state)
       return(
         <div class="Master_container">
@@ -56,21 +80,14 @@ constructor(props){
               </Zoom>
             </div>
 
-            <div class="new">
-              {
-                this.state.array.map((item,index)=>{
-                  return <div class={one}><p class="fourpara">{item}</p></div>
-                })
-              }
+            <div >
+              <div class={one}><p class="fourpara">Active Noise Cancellation for immersive sound.</p></div>
+              <div class={two} id="two"><p class="fourpara">Transparency mode for hearing what’s happening around you.</p></div>
+              <div class={three} id="three"><p class="fourpara">A customizable fit for all-day comfort.</p></div>
+              <div class={four} id="four"><p class="fourpara">Magic like you’ve never heard.</p></div>
            </div>
 
-           <div class="new">
-             {
-               this.state.array.map((item,index)=>{
-                 return <div class={one}><p class="fourpara">{item}</p></div>
-               })
-             }
-          </div>
+
 
 
         </div>
